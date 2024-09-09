752ee1ad4f4e8a03a52d5a4666651332a1290bee
Barış Bükümcüler <baris_bukumculer@hotmail.com>
2024-09-09 16:39:51 +0300

    Single page Angular investment money calculation application using signal and dependent module structure with certain components

---
+++ src/app/investment-input.model.ts
@@ -0,0 +1,13 @@
+export interface InvestmentInput {
+  initialInvestment: number;
+  duration: number;
+  expectedReturn: number;
+  annualInvestment: number;
+}
+
+//export type InvestmentInputType = {
+//   initialInvestment: number;
+//   duration: number;
+//   expectedReturn: number;
+//   annualInvestment: number;
+// };
